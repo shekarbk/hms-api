ea5fc932c05cc824afb09e901b49f90ba88f73ed
Chandrashekar BK <chandrashekar.bk@noria.no>
2020-10-13 15:54:57 +0200

    added junit test cases for booking service

--- src/main/java/com/spring/api/hms/service/RegistrationService.java
+++ src/main/java/com/spring/api/hms/service/RegistrationService.java
@@ -2,9 +2,12 @@ package com.spring.api.hms.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.spring.api.hms.enums.RoleEnum;
 import com.spring.api.hms.model.Registration;
 
+@Service
 public interface RegistrationService {
 	public void saveRegistrationDetails(Registration registration);
 
---
+++ src/test/java/com/spring/api/hms/service/impl/BookingServiceImplTest.java
@@ -0,0 +1,195 @@
+package com.spring.api.hms.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.spring.api.hms.entity.BookingDetailsEntity;
+import com.spring.api.hms.entity.UserDetailsEntity;
+import com.spring.api.hms.exception.NoRecordFoundException;
+import com.spring.api.hms.model.BookingDetails;
+import com.spring.api.hms.repository.BookingDetailsRepository;
+import com.spring.api.hms.repository.UserDetailsRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookingServiceImplTest {
+
+	@Mock
+	UserDetailsRepository userDetailsRepository;
+
+	@Mock
+	BookingDetailsRepository bookingDetailsRepository;
+
+	@InjectMocks
+	BookingServiceImpl BookingServiceImpl;
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testGetBookingDetailsByIdAndDate() {
+		BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+		UserDetailsEntity userDtlsObj1 = new UserDetailsEntity();
+		userDtlsObj1.setUserId(2);
+		bookingDetailsEntity.setDoctorId(userDtlsObj1);
+
+		UserDetailsEntity userDtlsObj2 = new UserDetailsEntity();
+		userDtlsObj2.setUserId(1);
+		bookingDetailsEntity.setPatientId(userDtlsObj2);
+
+		List<BookingDetailsEntity> bookingEntyList = new ArrayList<BookingDetailsEntity>();
+		bookingEntyList.add(bookingDetailsEntity);
+
+		Mockito.when(
+				bookingDetailsRepository.findByIdAndDate(Mockito.any(UserDetailsEntity.class), Mockito.anyString()))
+				.thenReturn(bookingEntyList);
+		List<BookingDetails> bookingVOList = BookingServiceImpl.getBookingDetailsByIdAndDate(2, "10-10-2020");
+
+		Mockito.verify(bookingDetailsRepository).findByIdAndDate(Mockito.any(UserDetailsEntity.class),
+				Mockito.anyString());
+		assertEquals(1, bookingVOList.size());
+	}
+
+	@Test
+	public void testCreateNewBooking() {
+		BookingDetails bookingDetails = new BookingDetails();
+		UserDetailsEntity userDtlsObj = new UserDetailsEntity();
+		BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+
+		Mockito.when(userDetailsRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(userDtlsObj));
+		Mockito.when(bookingDetailsRepository.save(Mockito.any(BookingDetailsEntity.class)))
+				.thenReturn(bookingDetailsEntity);
+		BookingServiceImpl.createNewBooking(bookingDetails);
+
+		Mockito.verify(bookingDetailsRepository).save(Mockito.any(BookingDetailsEntity.class));
+	}
+
+	@Test
+	public void testDeleteBooking() throws NoRecordFoundException {
+		Mockito.doNothing().when(bookingDetailsRepository).deleteById(Mockito.anyInt());
+		BookingServiceImpl.deleteBooking(1);
+		Mockito.verify(bookingDetailsRepository).deleteById(Mockito.anyInt());
+	}
+
+	@Test
+	public void testDeleteBookingWithExpectedException() throws NoRecordFoundException {
+		Assertions.assertThrows(NoRecordFoundException.class, () -> {
+			Mockito.doNothing().when(bookingDetailsRepository).deleteById(Mockito.anyInt());
+			BookingServiceImpl.deleteBooking(0);
+			Mockito.verify(bookingDetailsRepository).deleteById(Mockito.anyInt());
+		});
+	}
+
+	@Test
+	public void testGetBookingSummaryDetails() {
+		BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+
+		UserDetailsEntity userDtlsObj1 = new UserDetailsEntity();
+		userDtlsObj1.setUserId(2);
+		bookingDetailsEntity.setDoctorId(userDtlsObj1);
+
+		UserDetailsEntity userDtlsObj2 = new UserDetailsEntity();
+		userDtlsObj2.setUserId(1);
+		bookingDetailsEntity.setPatientId(userDtlsObj2);
+
+		Mockito.when(bookingDetailsRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(bookingDetailsEntity));
+		BookingServiceImpl.getBookingSummaryDetails(1);
+
+		Mockito.verify(bookingDetailsRepository).findById(Mockito.anyInt());
+	}
+
+	@Test
+	public void testUpdateBookingDetails() throws NoRecordFoundException {
+		UserDetailsEntity userDtlsObj = new UserDetailsEntity();
+		Mockito.when(userDetailsRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(userDtlsObj));
+
+		BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+
+		UserDetailsEntity userDtlsObj1 = new UserDetailsEntity();
+		userDtlsObj1.setUserId(2);
+		bookingDetailsEntity.setDoctorId(userDtlsObj1);
+
+		UserDetailsEntity userDtlsObj2 = new UserDetailsEntity();
+		userDtlsObj2.setUserId(1);
+		bookingDetailsEntity.setPatientId(userDtlsObj2);
+
+		Mockito.when(bookingDetailsRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(bookingDetailsEntity));
+
+		Mockito.when(bookingDetailsRepository.save(Mockito.any(BookingDetailsEntity.class)))
+				.thenReturn(bookingDetailsEntity);
+
+		BookingDetails bookingDetails = new BookingDetails();
+		BookingServiceImpl.updateBookingDetails(bookingDetails);
+
+		Mockito.verify(bookingDetailsRepository).save(Mockito.any(BookingDetailsEntity.class));
+
+	}
+
+	@Test
+	public void testUpdateBookingDetailsWithExpectedException() {
+		Assertions.assertThrows(NoRecordFoundException.class, () -> {
+			Mockito.when(userDetailsRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+
+			BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+
+			UserDetailsEntity userDtlsObj1 = new UserDetailsEntity();
+			userDtlsObj1.setUserId(2);
+			bookingDetailsEntity.setDoctorId(userDtlsObj1);
+
+			UserDetailsEntity userDtlsObj2 = new UserDetailsEntity();
+			userDtlsObj2.setUserId(1);
+			bookingDetailsEntity.setPatientId(userDtlsObj2);
+
+			Mockito.when(bookingDetailsRepository.findById(Mockito.anyInt()))
+					.thenReturn(Optional.of(bookingDetailsEntity));
+
+			Mockito.when(bookingDetailsRepository.save(Mockito.any(BookingDetailsEntity.class)))
+					.thenReturn(bookingDetailsEntity);
+
+			BookingDetails bookingDetails = new BookingDetails();
+			BookingServiceImpl.updateBookingDetails(bookingDetails);
+
+			Mockito.verify(bookingDetailsRepository).save(Mockito.any(BookingDetailsEntity.class));
+		});
+	}
+	
+	@Test
+	public void testGetAppointmentDetailsByDate() {
+		BookingDetailsEntity bookingDetailsEntity = new BookingDetailsEntity();
+		UserDetailsEntity userDtlsObj1 = new UserDetailsEntity();
+		userDtlsObj1.setUserId(2);
+		bookingDetailsEntity.setDoctorId(userDtlsObj1);
+
+		UserDetailsEntity userDtlsObj2 = new UserDetailsEntity();
+		userDtlsObj2.setUserId(1);
+		bookingDetailsEntity.setPatientId(userDtlsObj2);
+
+		List<BookingDetailsEntity> bookingEntyList = new ArrayList<BookingDetailsEntity>();
+		bookingEntyList.add(bookingDetailsEntity);
+		
+		Mockito.when(bookingDetailsRepository.findBybookedDate(Mockito.anyString())).thenReturn(bookingEntyList);
+		
+		List<BookingDetails> bookingVOList = new ArrayList<BookingDetails>();
+		BookingDetails bookingDetails = new BookingDetails();
+		bookingVOList.add(bookingDetails);
+		
+		BookingServiceImpl.getAppointmentDetailsByDate("10-10-2010");
+		Mockito.verify(bookingDetailsRepository).findBybookedDate(Mockito.anyString());
+		
+		assertEquals(1, bookingVOList.size());
+	}
+}
